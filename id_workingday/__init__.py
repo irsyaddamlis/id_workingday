9dc32df6c8df1231056096bf5062eeeed36a5621
Irsyad Damlis <irsyad.damlis@gmail.com>
2026-05-15 15:34:33 +0700

    Revise __init__.py

--- id_workingday/__init__.py
+++ id_workingday/__init__.py
@@ -1,1 +1,10 @@
-from .calendar import total_working_day, working_date, working_day
+from .calendar import (total_working_day, working_date, working_day)
+
+__all__ = [
+    "working_date",
+    "total_working_day",
+    "working_day",
+]
+
+__version__ = "1.0.0"
+__author__ = "Irsyad Damlis"
